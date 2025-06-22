0fe2115ae6b33a6668ade5a83f59e6986ed24161
tyronecopelandjr <tycopelandjr@gmail.com>
2025-06-22 05:47:28 +0000

    First commit

---
+++ .devcontainer/app.jsx
@@ -0,0 +1,29 @@
+import "bootstrap";
+import "./style.css";
+
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while 
+
+  function getRandomInt(a, b) {
+        return Math.floor(Math.random() * (b - a + 1)) + a;
+    }
+
+  function getRandom(anyArray) {
+        let max = who.length - 1;
+        let min = 0
+        let random = getRandomNumber(min, max);
+        return anyArray[random];
+    }
+
+window.onload = function () {
+        //write your code here
+        console.log("Hello Rigo from the console!");
+        document.querySelector('#excuse').innerHTML = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when)
+    };
+
